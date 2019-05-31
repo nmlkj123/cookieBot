3afe5251f2d0dcd11ea7b3df5920df8c70f7079a
nmlkj123 <51207216+nmlkj123@users.noreply.github.com>
2019-05-31 15:16:34 +0900

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,6 +1,9 @@
 import discord
-import os
+
+
+import datetime
 import urllib.request
+import random
 import bs4
 
 
@@ -16,33 +19,44 @@ async def on_ready():
     await client.change_presence(activity=discord.Game(name="말하는 쿠키봇"))
 
 
+
 @client.event
 async def on_message(message):
-    if message.content.startswith('!실검'):
+    if message.content.startswith('!롤각'):
+        randomNum = random.randrange(1, 3)
+        if randomNum == 1:
+            await message.channel.send(
+                embed=discord.Embed(description="롤각 가즈아~ (* >ω<) (＞Д＜)ゝ", colour=discord.Colour.green()))
+            return
+        else:
+            await message.channel.send(
+                embed=discord.Embed(description="응 오늘은아니야~ ಥ_ಥ (╥_╥)", colour=discord.Colour.green()))
+            return
+
+    if message.content.startswith('!실검') :
         url = "https://www.naver.com/"
-    html = urllib.request.urlopen(url)
+        html = urllib.request.urlopen(url)
 
-    bsObj = bs4.BeautifulSoup(html, "html.parser")
-    realTimeSerach1 = bsObj.find('div', {'class': 'ah_roll_area PM_CL_realtimeKeyword_rolling'})
-    realTimeSerach2 = realTimeSerach1.find('ul', {'class': 'ah_l'})
-    realTimeSerach3 = realTimeSerach2.find_all('li')
+        bsObj = bs4.BeautifulSoup(html, "html.parser")
+        realTimeSerach1 = bsObj.find('div', {'class': 'ah_roll_area PM_CL_realtimeKeyword_rolling'})
+        realTimeSerach2 = realTimeSerach1.find('ul', {'class': 'ah_l'})
+        realTimeSerach3 = realTimeSerach2.find_all('li')
 
-    embed = discord.Embed(
-        title='네이버 실시간 검색순위',
-        description='1위~10위',
-        colour=discord.Colour.green()
-    )
-    for i in range(0, 10):
-        realTimeSerach4 = realTimeSerach3[i]
-        realTimeSerach5 = realTimeSerach4.find('span', {'class': 'ah_k'})
-        realTimeSerach = realTimeSerach5.text.replace(' ', '')
-        realURL = 'https://search.naver.com/search.naver?ie=utf8&query=' + realTimeSerach
-        print(realTimeSerach)
-        embed.add_field(name=str(i + 1) + '위', value='\n' + '[%s](<%s>)' % (realTimeSerach, realURL),
-                        inline=False)  # [텍스트](<링크>) 형식으로 적으면 텍스트 하이퍼링크 만들어집니다
+        embed = discord.Embed(
+            title='네이버 실시간 검색순위',
+            description='1위~10위',
+            colour=discord.Colour.green()
+        )
+        for i in range(0, 10):
+            realTimeSerach4 = realTimeSerach3[i]
+            realTimeSerach5 = realTimeSerach4.find('span', {'class': 'ah_k'})
+            realTimeSerach = realTimeSerach5.text.replace(' ', '')
+            realURL = 'https://search.naver.com/search.naver?ie=utf8&query=' + realTimeSerach
+            print(realTimeSerach)
+            embed.add_field(name=str(i + 1) + '위', value='\n' + '[%s](<%s>)' % (realTimeSerach, realURL),
+                            inline=False)  # [텍스트](<링크>) 형식으로 적으면 텍스트 하이퍼링크 만들어집니다
 
-    await message.channel.send(embed=embed)
-
+        await message.channel.send( embed=embed)
 
     if message.content.startswith("!명령어"):
         embed = discord.Embed(
@@ -50,26 +64,27 @@ async def on_message(message):
             description='-쿠키봇의 명령어\n\n ',
             colour=discord.Colour.blue()
         )
-
+        dtime = datetime.datetime.now()
         embed.add_field(name='!롤 아이디', value='롤op.gg 전적검색 결과를 보여줍니다 참고- 언랭은 정보따위 제공안합니다.', inline=True)
         embed.add_field(name='!핑', value='지연시간확인. ', inline=False)
         embed.add_field(name='!투표 제목/내용/내용...', value='투표함을 만듭니다. 이모지를 클릭하여 투표를 진행합니다.', inline=False)
-        embed.add_field(name='!실검', value='네이버 실시간 검색순위를 1위부터 10위까지 보여줍니다.', inline=False)
-
+        embed.add_field(name='!실검', value='네이버 실시간 검색순위를 1위부터 20위까지 보여줍니다.', inline=False)
+        embed.set_footer(text=str(dtime.year) + "년 " + str(dtime.month) + "월 " + str(dtime.day) + "일 " + str(
+            dtime.hour) + "시 " + str(dtime.minute) + "분")
         await message.channel.send(embed=embed)
 
     if message.content.startswith("!투표"):
 
         vote = message.content[4:].split("/")
 
-        await message.channel.send("✉투표 - " + vote[0])
-        for i in range(1, len(vote)):
-            choose = await  message.channel.send("```" + vote[i] + "```")
+        await message.channel.send("✉투표 - "+vote[0])
+        for i in range(1,len(vote)):
+            choose= await  message.channel.send("```"+vote[i]+"```")
             await choose.add_reaction('👍')
 
     if message.content.startswith("!핑"):
-        ping = '%.2f' % (1000 * (client.latency))
-        await  message.channel.send(ping + "ms")
+        ping = '%.2f' % (1000 * (client.latency ))
+        await  message.channel.send(ping )
 
     if message.content.startswith("!롤"):
         learn = message.content[3:].split(" ")
@@ -79,7 +94,7 @@ async def on_message(message):
             loc = ""
             for l in range(0, len(learn), 1):
                 loc += learn[l]
-            location = loc
+            location=loc
         enc_location = urllib.parse.quote(location)
         print(enc_location)
         url = "http://www.op.gg/summoner/userName=" + enc_location
@@ -89,17 +104,17 @@ async def on_message(message):
         id1 = bsObj.find("div", {"class": "Profile"})
 
         if id1 is None:
-            await message.channel.send(embed=discord.Embed(description="입력하신 아이디의 정보가없습니다", colour=discord.Colour.green()))
+            await message.channel.send(embed=discord.Embed(description="입력하신 아이디의 정보가없습니다",colour=discord.Colour.green()))
             return
         id2 = id1.find("div", {"class": "Information"})
         id3 = id2.find("span", {"class": "Name"})
         id4 = id3.text
 
         imaget1 = bsObj.find("div", {"class": "SummonerRatingMedium"})
-        imaget2 = imaget1.find("img", {"class": "Image"})
+        imaget2 = imaget1.find("img",{"class": "Image"})
         imaget3 = imaget2.get('src')
-        imagetCut = imaget3.split("/")
-        imageThumbnail = imagetCut[len(imagetCut) - 1]
+        imagetCut=imaget3.split("/")
+        imageThumbnail=imagetCut[len(imagetCut) - 1]
 
         print(imageThumbnail)
         imagec1 = bsObj.find("div", {"class": "ProfileIcon"})
@@ -111,7 +126,7 @@ async def on_message(message):
         rank1 = bsObj.find("div", {"class": "TierRankInfo"})
         rank2 = rank1.find("div", {"class": "TierRank"})
         rank4 = rank2.text  # 티어표시 (브론즈1,2,3,4,5 등등)
-        if rank4 == '':
+        if rank4=='':
             rank4 = rank1.find("div", {"class": "TierRank unranked"}).text
         print(rank4)
         if rank4.strip() != 'Unranked':
@@ -134,14 +149,14 @@ async def on_message(message):
 
         channel = message.channel
         embed = discord.Embed(
-            title=id4 + '님의 롤 정보',
+            title=id4+'님의 롤 정보',
             description='롤 정보입니다.',
 
             colour=discord.Colour.red()
         )
 
-        embed.set_thumbnail(url="http://opgg-static.akamaized.net/images/medals/" + imageThumbnail)
-        embed.set_image(url="http://opgg-static.akamaized.net/images/profile_icons/" + imageIcon)
+        embed.set_thumbnail(url="http://opgg-static.akamaized.net/images/medals/"+imageThumbnail)
+        embed.set_image(url="http://opgg-static.akamaized.net/images/profile_icons/"+imageIcon )
         if rank4.strip() == 'Unranked':
             embed.add_field(name='당신의 티어', value=rank4, inline=False)
             embed.add_field(name='-당신은 언랭-', value="언랭은 정보제공 따위 안합니다.", inline=False)
@@ -152,6 +167,8 @@ async def on_message(message):
             embed.add_field(name='당신의 승,패 정보', value=winlose2txt + " " + winlose2_1txt, inline=False)
             embed.add_field(name='당신의 승률', value=winlose2_2txt, inline=False)
             await message.channel.send(embed=embed)
+
+
 
 
 access_token = os.environ["BOT_TOKEN"]
