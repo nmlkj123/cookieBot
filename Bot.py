b02ef19cfa0fa54ff63ff6a408f90115fb62cc98
nmlkj123 <51207216+nmlkj123@users.noreply.github.com>
2019-06-10 20:01:51 +0900

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -24,44 +24,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith("!복권"):
-        Text = ""
-        number = [1, 2, 3, 4, 5, 6, 7]
-        count = 0
-        for i in range(0, 7):
-            num = random.randrange(1, 46)
-            number[i] = num
-            if count >= 1:
-                for i2 in range(0, i):
-                    if number[i] == number[i2]:  # 만약 현재랜덤값이 이전숫자들과 값이 같다면
-                        numberText = number[i]
-                        print("작동 이전값 : " + str(numberText))
-                        number[i] = random.randrange(1, 46)
-                        numberText = number[i]
-                        print("작동 현재값 : " + str(numberText))
-                        if number[i] == number[i2]:  # 만약 다시 생성한 랜덤값이 이전숫자들과 또 같다면
-                            numberText = number[i]
-                            print("작동 이전값 : " + str(numberText))
-                            number[i] = random.randrange(1, 46)
-                            numberText = number[i]
-                            print("작동 현재값 : " + str(numberText))
-                            if number[i] == number[i2]:  # 만약 다시 생성한 랜덤값이 이전숫자들과 또 같다면
-                                numberText = number[i]
-                                print("작동 이전값 : " + str(numberText))
-                                number[i] = random.randrange(1, 46)
-                                numberText = number[i]
-                                print("작동 현재값 : " + str(numberText))
-
-            count = count + 1
-            Text = Text + "  " + str(number[i])
-
-        print(Text.strip())
-        embed = discord.Embed(
-            title="가즈아!",
-            description=Text.strip(),
-            colour=discord.Color.red()
-        )
-        await message.channel.send(embed=embed)
+        
     if message.content.startswith('!냥냥'):
         
         embed = discord.Embed(
