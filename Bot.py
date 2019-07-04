92ec3e65f2a135e80e6dee0be8d2b2418b9b65a1
nmlkj123 <51207216+nmlkj123@users.noreply.github.com>
2019-07-04 17:47:53 +0900

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -132,7 +132,7 @@ async def on_message(message):
             skill+=skillc2[i].find('span').text
 
         embed = discord.Embed(
-            title=name,
+            title=":8851_league_of_legends: "+name,
             description=tier,
 
             colour=discord.Colour.green()
@@ -550,7 +550,7 @@ async def on_message(message):
         channel = message.channel
         embed = discord.Embed(
             title=id4+'님의 롤 정보',
-            description='롤 정보입니다.',
+            description=':8851_league_of_legends: 롤 정보입니다.',
 
             colour=discord.Colour.red()
         )
