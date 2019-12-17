1c5d2273e2e96bcc32ce4f0634c8fde7f31c6271
nmlkj123 <51207216+nmlkj123@users.noreply.github.com>
2019-12-17 15:37:05 +0900

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -513,8 +513,8 @@ async def on_message(message):
         await message.channel.send("✉투표 - " + vote[0])
         for i in range(1, len(vote)):
             
-            choose = await message.channel.send(vote[i])
-            await message.add_reaction(choose,'👍')
+            choose = await client.send_message(message.channel,vote[i])
+            await client.add_reaction(choose,'👍')
         return
     if message.content.startswith("!핑"):
         ping = '%.2f' % (1000 * (client.latency))
